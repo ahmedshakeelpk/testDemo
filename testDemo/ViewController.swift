a9dbe63fe774e4b026601f02a5a76da229d6a63b
ahmedshakeelpk <ahmedshakeelpk@gmail.com>
2024-07-05 01:18:40 +0500

    bug fixmerge file

--- testDemo/ViewController.swift
+++ testDemo/ViewController.swift
@@ -8,30 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         //test code
-//        comment1
-//        comment3
-//        comment4
-//        comment5
-//        comment6
-//        comment7
-//        comment8
-//        comment9
+        //        comment1
+        //        comment3
+        //        comment4
+        //        comment5
+        //        comment6
+        //        comment7
+        //        comment8
+        //        comment9
         //comment12
-        //comment13
-        //comment14
-        //comment15
-        //comment16
-
-
+        
         
     }
-
-
 }
-
